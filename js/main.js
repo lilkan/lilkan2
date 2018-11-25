8c304ecca01482cdbf0400872560e7da9b6f7a13
lilkan <doooomen@gmail.com>
2018-11-25 04:29:59 +0300

    acco

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ let mobileViewOpen = document.getElementById('mobile__view_btn');
 let mobileView = document.getElementById('mobile__view');
 let mobileViewClose = document.getElementById('close__button_mob');
 
-console.log (mobileView);
-console.log (mobileViewOpen);
-console.log (mobileViewClose);
-
 mobileViewOpen.addEventListener('click', function() {
      mobileView.style.display = "flex";
 }
@@ -14,4 +10,17 @@ mobileViewOpen.addEventListener('click', function() {
 mobileViewClose.addEventListener('click', function(){
     mobileView.style.display = "none";
 
+})
+
+
+$(document).ready(()=>{
+    $('.team-acco__item').on('click', (e) => {
+        e.preventDefault();
+        let psuh = $(e.currentTarget)
+        psuh.toggleClass('team-acco__active');
+    })
+    $('.menu-acco__item').on('click', (e) => {
+        let push = $(e.currentTarget)
+        push.find('.menu-acco__content').toggleClass('menu-acco__content-active');
+    })
 })
