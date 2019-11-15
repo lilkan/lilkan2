de922ea1fafb7e907ca1ad2c612db37334afe500
lilkan <doooomen@gmail.com>
2019-11-15 21:57:36 +0300

    gulp

---
+++ README.md
@@ -0,0 +1,7 @@
+###Как запустить
+
+```
+git clone https://github.com/lilkan/lilkan2.git
+npm i
+gulp
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,78 @@
-let gulp = require('gulp'),
-    cleanCSS = require('gulp-clean-css'),
-    watch = require('gulp-watch'),
-    postcss = require('gulp-postcss'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const gulpWebpack = require('webpack-stream');
 
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/pug/*.pug',
+    },
+    styles: {
+        src: 'src/css/*.scss',
+        dest: 'build/assets/css'
+    },
+    scrypts: {
+        src: 'src/js/*.js',
+        dest: 'build/assets/js',
+        json: 'src/js/*.json'
+    }
+}
+//move json
+function json () {
+    return gulp.src(paths.scrypts.json)
+        .pipe(gulp.dest(paths.root))
+}
+//convert pug
+function html () {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({pretty:true}))
+        .pipe(gulp.dest(paths.root));
+};
+// convert sass
+function css(){
+    return gulp.src('./src/css/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+};
+// clean
+function clean(){
+    return del(paths.root);
+};
+//gulp watcher
+function watch(){
+    gulp.watch(paths.styles.src, css);
+    gulp.watch(paths.templates.pages, html);
+    gulp.watch(paths.scrypts.src, scrypts);
+    gulp.watch(paths.scrypts.src, json);
 
-sass.compiler = require('node-sass');
+}
+//local host + livereload
+function server(){
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+function scrypts () {
+    return gulp.src(paths.scrypts.src)
+        .pipe(gulpWebpack(webpackConfig))
+        .pipe(gulp.dest(paths.scrypts.dest));
+  }
+exports.server = server;
+exports.html = html;
+exports.css = css;
+exports.clean = clean;
+exports.scrypts = scrypts;
+exports.json = json;
 
-
-gulp.task('default', () => {
-  return gulp.src('scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-      }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('test_two/css'));
-});
-
-gulp.task('watch', () => {
-  gulp.watch('scss/**/*.scss', gulp.parallel('default'))
-})
+gulp.task('default', gulp.series(
+    gulp.parallel(server)
+));
